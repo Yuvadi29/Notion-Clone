c26905c908fa8d2e5b4d1552d825380b81948608
Yuvadi29 <adityatrivedi612@gmail.com>
2024-07-20 16:03:32 +0530

    Building the Header Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import {
   ClerkProvider,
 } from '@clerk/nextjs'
+import Header from "@/components/Header";
 
 
 export const metadata: Metadata = {
@@ -19,7 +20,12 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          {children}
+          <Header />
+
+          <div className="flex min-h-screen">
+            {/* Sidebar */}
+            <div className="flex-1 p-4 bg-gray-100 overflow-y-auto scrollbar-hide" >{children}</div>
+          </div>
         </body>
       </html>
     </ClerkProvider>
